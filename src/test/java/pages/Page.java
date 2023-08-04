a8b6d0998ad14e8da73a18f59c05bd9a977d34a0
Yasemin Eric <yasemineric6@gmail.com>
2023-08-05 00:07:01 +0200

    05.08.2023_Yasemin

--- src/test/java/pages/Page.java
+++ src/test/java/pages/Page.java
@@ -5,17 +5,17 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 import java.util.List;
+
+
+
+
+
+
+
 public class Page {
     public Page(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
-
-
-
-
-
-
-
 
 
 
