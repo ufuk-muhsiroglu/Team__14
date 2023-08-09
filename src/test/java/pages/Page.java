cb195ad8f8aa06b1af4c9f80d647a4d383fe6cab
Gönül Obuz <gonulbuyuk2@hotmail.com>
2023-08-10 02:31:05 +0300

    10.08gönül

--- src/test/java/pages/Page.java
+++ src/test/java/pages/Page.java
@@ -13,12 +13,12 @@ import java.util.List;
 
 
 public class Page {
-    public Page(){
-        PageFactory.initElements(Driver.getDriver(),this);
+
+    public Page() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
     
-
-
-
 
 
     //Beyza Çolak 24-64 satır arası
@@ -195,22 +195,22 @@ public class Page {
 
 
 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+  
+  
+  
+  
+  
 
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-  
-  
-  
-  
-  
    // Ali İsaoğlu 214-264 arası
     @FindBy(xpath ="//span[text()='Register']")  public WebElement registerButonu;
     //Register'a tikladiktan sonra acilan ilk sayfadaki WebElementler
@@ -316,7 +316,7 @@ public class Page {
     @FindBy(xpath = "(//input)[1]") public WebElement signInUsername;
     @FindBy(xpath = "(//input)[2]") public WebElement signInPassword;
     @FindBy(xpath = "(//button)[1]") public WebElement signInSubmit;
-    @FindBy(xpath = "//*[@class='login logout inline-type']") public WebElement signOutButonu;
+    @FindBy(xpath = "//*[@class='login logout inline-type']") public WebElement siaagnOutButonu;
     //US14
     @FindBy(xpath = "//*[text()='Edit Your Shipping Address']") public WebElement editShipping;
     @FindBy(xpath = "//*[@id='main']/div/div/div/div/div/div/nav/ul/li[5]/a") public WebElement adresler;
@@ -403,7 +403,7 @@ public class Page {
     @FindBy(xpath = "(//*[@name='product_cats[]'])[1]")  public WebElement categoriesFirstBox;
     @FindBy(xpath = "//*[@id='wcfm-products_wrapper']")  public WebElement productDetails1;
     @FindBy(css = "[id=username]")  public WebElement signInUserNameOrEmaill;
-    @FindBy(css = "[id=password]")  public WebElement signInPassword;
+    @FindBy(css = "[id=password]")  public WebElement signInPassword1;
     @FindBy(xpath = "(//*[@href='https://allovercommerce.com/my-account-2/'])[1]")  public WebElement myAccount;
     @FindBy(xpath = "(//*[@href='https://allovercommerce.com/store-manager/'])")  public WebElement storeManager1;
 
